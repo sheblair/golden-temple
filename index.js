fa95293da6a909ce45d37b8af54b9d49193d11b7
sheblair <66489366+sheblair@users.noreply.github.com>
2023-05-15 11:11:23 -0400

    rm more comments

--- index.js
+++ index.js
@@ -4,11 +4,8 @@ let i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
-    /* toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
     this.classList.toggle("active");
 
-    /* toggle between hiding and showing the active panel */
     const panel = this.nextElementSibling;
     if (panel.style.display === "block") {
       panel.style.display = "none";
